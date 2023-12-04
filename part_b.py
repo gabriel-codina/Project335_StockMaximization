c36f2b49e81003338e7701cfb62fd787b45a741a
장아현 <jang-ahyeon@jang-ahyeon-ui-MacBookPro.local>
2023-12-03 22:33:12 -0800

    write results on output.txt

--- part_b.py
+++ part_b.py
@@ -7,6 +7,7 @@ def main():
   stock_list=[]
   
   f = open("input_project2.txt", "r")
+  results = []
   for x in f:
     #split input in 3 lines(# of stocks, stock_list, and max_budget)
     if line_count==0 and x.strip().isdigit():
@@ -17,9 +18,12 @@ def main():
     elif line_count==2:
       line_count=0
       max_budget=int(x)
-      print(dynamic_opt_stocks2(stock_list,max_budget))
+      result = total_stocks(dynamic_opt_stocks(stock_list,max_budget))
+      print(result)
+      results.append(result)
+    write_to_output(results)
 
-''' Old Code
+
 def dynamic_opt_stocks(stock_list, max_budget):
   #using a dynamic algorithm to find the best combination of stocks
   solution=[]
@@ -34,20 +38,6 @@ def dynamic_opt_stocks(stock_list, max_budget):
       max_budget-=best_val_option[1]
     stock_list.remove(best_val_option)
   return solution
-'''
-
-def dynamic_opt_stocks2(stock_list, max_budget):
-  n=len(stock_list)
-  S = [[0 for x in range(max_budget+1)] for x in range(n+1)]
-  for i in range (0, n+1):
-    for w in range (0, max_budget+1):
-      if i==0 or w==0:
-        S[i][w]=0
-      elif stock_list[i-1][1]<=w: 
-        S[i][w] = max(stock_list[i-1][0]+S[i-1][w-stock_list[i-1][1]], S[i-1][w])
-      else:
-        S[i][w]=S[i-1][w]
-  return S[n][max_budget]
 
 def total_stocks(candidate):
   #get the total number of stocks in the candidate
@@ -56,6 +46,10 @@ def total_stocks(candidate):
     total+=item[0]
   return total
 
+def write_to_output(results):
+  with open("output_b.txt", "w") as output_b:
+    for i in results:
+      output_b.write(str(i) + "\n")
 
 
 if __name__ == "__main__":
