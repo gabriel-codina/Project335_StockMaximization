6bbd2686ce76fddca56ead444ef638a541571b78
장아현 <jang-ahyeon@jang-ahyeon-ui-MacBookPro.local>
2023-12-03 23:15:35 -0800

    change the dp code

--- part_b.py
+++ part_b.py
@@ -6,7 +6,7 @@ def main():
   max_budget=0
   stock_list=[]
   
-  f = open("input_project2.txt", "r")
+  f = open("input_b.txt", "r")
   results = []
   for x in f:
     #split input in 3 lines(# of stocks, stock_list, and max_budget)
@@ -18,40 +18,33 @@ def main():
     elif line_count==2:
       line_count=0
       max_budget=int(x)
-      result = dynamic_opt_stocks2(stock_list,max_budget)
+      result = total_stocks(dynamic_opt_stocks(stock_list,max_budget))
       print(result)
       results.append(result)
     write_to_output(results)
 
-''' Old Code
-def dynamic_opt_stocks(stock_list, max_budget):
-  #using a dynamic algorithm to find the best combination of stocks
-  solution=[]
-  #evaluate stock value and add it as a third item
-  for stock in stock_list:
-    stock.append(stock[0]/stock[1])
-  #while their is items in our list and we still have a budget get the highest value stock option
-  while stock_list and max_budget > 0:
-    best_val_option=max(stock_list, key=lambda x: x[2])#get highest value set
-    if best_val_option[1]<=max_budget:
-      solution.append([best_val_option[0],best_val_option[1]])
-      max_budget-=best_val_option[1]
-    stock_list.remove(best_val_option)
-  return solution
-'''
 
-def dynamic_opt_stocks2(stock_list, max_budget):
-  n=len(stock_list)
-  S = [[0 for x in range(max_budget+1)] for x in range(n+1)]
-  for i in range (0, n+1):
-    for w in range (0, max_budget+1):
-      if i==0 or w==0:
-        S[i][w]=0
-      elif stock_list[i-1][1]<=w: 
-        S[i][w] = max(stock_list[i-1][0]+S[i-1][w-stock_list[i-1][1]], S[i-1][w])
-      else:
-        S[i][w]=S[i-1][w]
-  return S[n][max_budget]
+def dynamic_opt_stocks(stock_list, max_budget):
+    #using a dynamic algorithm to find the best combination of stocks
+    n = len(stock_list)
+    dp = [[0] * (max_budget + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(max_budget + 1):
+            dp[i][j] = dp[i - 1][j]
+            if stock_list[i - 1][1] <= j:
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - stock_list[i - 1][1]] + stock_list[i - 1][0])
+
+    # finding the optimal combination of stocks through backtracking
+    solution = []
+    while n and max_budget > 0:
+        if dp[n][max_budget] != dp[n - 1][max_budget]:
+            solution.append(stock_list[n - 1])
+            max_budget -= stock_list[n - 1][1]
+        n -= 1
+
+    return solution
+
 
 def total_stocks(candidate):
   #get the total number of stocks in the candidate
